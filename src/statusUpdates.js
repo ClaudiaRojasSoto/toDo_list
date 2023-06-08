47a7746aff7200167e1ffa4b6a5c801dcd9eab84
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-06-07 23:09:01 -0400

    Implement the updateTaskStatus on statusUpdates.js

--- src/statusUpdates.js
+++ src/statusUpdates.js
@@ -1,6 +1,7 @@
+import { saveTasks } from './taskFunctions.js';
+
 export const updateTaskStatus = (tasks, index, completed) => {
     tasks[index].completed = completed;
     saveTasks(tasks);
-    renderTasks();
   };
   
